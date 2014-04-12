42004b52646ebd6af254ed7cef27732d47286cb7
Adam Borzecki <adam@bnotions.com>
2014-04-12 11:00:47 -0400

    First commit

---
+++ asteroid-defense/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  asteroid-defense
+//
+//  Created by Adam Borzecki on 2014-04-12.
+//  Copyright (c) 2014 Adam Borzecki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
